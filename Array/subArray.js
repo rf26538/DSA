1a6bd05ef8506519932d0cc0be03e8c92ebc3a1d
Rehan Fazal <rf26538@gmail.com>
2025-09-08 01:53:03 +0530

    sub Array

--- Array/subArray.js
+++ Array/subArray.js
@@ -1,12 +1,11 @@
 function printSubarrays(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      let sub = [];
-      for (let j = i; j < arr.length; j++) {
-        sub.push(arr[j]);
-        console.log([...sub]);
-      }
+  for (let i = 0; i < arr.length; i++) {
+    let sub = [];
+    for (let j = i; j < arr.length; j++) {
+      sub.push(arr[j]);
+      console.log(sub);
     }
   }
-  
-  printSubarrays([1, 2, 3]);
-  
+}
+
+printSubarrays([1, 2, 3]);
